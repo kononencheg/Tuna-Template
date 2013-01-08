7efe8836e9825923e9c16aae18b30706767b9d1f
Sergey Kononenko <kononencheg@gmail.com>
2013-01-08 15:59:44 +0400

    I`m good.

--- src.d
+++ src.d
@@ -1,18 +1,11 @@
 
 lib/tt/tt.js
 lib/tt/template.js
-
-lib/tt/rules/rule.js
-lib/tt/rules/rules.js
+lib/tt/template-unit.js
+lib/tt/template-rule.js
 
 lib/tt/data/path-evaluator.js
 lib/tt/data/data-node.js
-lib/tt/data/data.js
-
-lib/tt/tree/i-template-node.js
-lib/tt/tree/template-root.js
-lib/tt/tree/template-node.js
-lib/tt/tree/tree.js
 
 lib/tt/view/helpers/i-template-view-helper.js
 lib/tt/view/helpers/simple-view-helper.js
